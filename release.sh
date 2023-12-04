f867f634c773972875d93c1e68c8e623c4700b98
Dude112113 <curtisdude12@gmail.com>
2023-12-04 09:09:38 +0000

    `release.sh` now zips release.

--- release.sh
+++ release.sh
@@ -2,22 +2,23 @@
 set -e
 
 rm -f -r ./release/
-
-echo - Copying files to ./release/
-
 mkdir ./release/
-cp protolua.py ./release/protolua.py
-cp -r lua_template ./release/lua_template
-cp -r lua_typing ./release/lua_typing
 
-mkdir ./release/build/
-cp build/protolua.wasm ./release/build/protolua.wasm
+echo - Copying files to ./release/ProtoLua/
+
+mkdir ./release/ProtoLua/
+cp protolua.py ./release/ProtoLua/protolua.py
+cp -r lua_template ./release/ProtoLua/lua_template
+cp -r lua_typing ./release/ProtoLua/lua_typing
+
+mkdir ./release/ProtoLua/build/
+cp build/protolua.wasm ./release/ProtoLua/build/protolua.wasm
 
 echo
 
 echo - Building \'protolua.py\' for Linux
 pyinstaller --workpath ./build/protolua-linux --specpath ./build/ -F protolua.py
-cp ./dist/protolua ./release/protolua
+cp ./dist/protolua ./release/ProtoLua/protolua
 
 echo
 
@@ -26,12 +27,14 @@ if test -f /proc/sys/fs/binfmt_misc/WSLInterop; then
 echo - Building \'protolua.py\' for Windows \(WSL environment\)
 BUILT_WINDOWS=1
 pyinstaller.exe --workpath ./build/protolua-win --specpath ./build/ -F protolua.py
-cp dist/protolua.exe ./release/protolua.exe
+cp dist/protolua.exe ./release/ProtoLua/protolua.exe
 fi
 
 echo
 
-echo - Built ./release/
+echo - Built ./release/ProtoLua/
 if [[ $BUILT_WINDOWS -ne 1 ]]; then
 echo WARN: Windows binary for \'protolua.py\' will need to be built on windows or wsl!
 fi
+
+cd ./release && zip -r ProtoLua.zip ./ProtoLua/

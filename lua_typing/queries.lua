03ddc44d022ce909fb1247b087c716e4b5e9dc65
Dude112113 <curtisdude12@gmail.com>
2023-12-04 08:30:22 +0000

    Fixed whitespace

--- lua_typing/queries.lua
+++ lua_typing/queries.lua
@@ -9,24 +9,24 @@
 ---@field ship_get_position_x fun(): number
 ---@field ship_get_position_y fun(): number
 ---@field ship_get_position_z fun(): number
- ---@field ship_get_velocity_x fun(): number
+---@field ship_get_velocity_x fun(): number
 ---@field ship_get_velocity_y fun(): number
 ---@field ship_get_velocity_z fun(): number
- ---@field ship_get_orientation_x fun(): number
+---@field ship_get_orientation_x fun(): number
 ---@field ship_get_orientation_y fun(): number
 ---@field ship_get_orientation_z fun(): number
 ---@field ship_get_orientation_w fun(): number
- ---@field ship_get_angularvelocity_x fun(): number
+---@field ship_get_angularvelocity_x fun(): number
 ---@field ship_get_angularvelocity_y fun(): number
 ---@field ship_get_angularvelocity_z fun(): number
- ---@field engine_get_fuel_amount fun(): number
+---@field engine_get_fuel_amount fun(): number
 ---@field engine_get_fuel_capacity fun(): number
 ---@field engine_get_throttle fun(): number
 ---@field radar_get_target_count fun(): number
 ---@field radar_get_target_distance fun(index: number): number
 ---@field radar_get_target_type fun(index: number): number
 ---@field radar_get_target_id fun(index: number): number
-  ---@field gun0_get_bearing fun(): number
+---@field gun0_get_bearing fun(): number
 ---@field gun0_get_elevation fun(): number
 ---@field gun0_get_refiretime fun(): number
 ---@field gun1_get_bearing fun(): number
